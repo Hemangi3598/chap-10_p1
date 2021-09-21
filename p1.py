78eccab1e0d6897bcbe265f39bcc7e1f1417efa0
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-21 13:14:44 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,13 @@
+# wapp to accept as input integers
+# print if the number is even or odd
+
+print("welcome")
+num = int(input(" enter an integer "))
+if num % 2 == 0:
+	msg = "even"
+else:
+	msg = "odd"
+print(msg)
+print("bye")
+
+# without any exception handling
